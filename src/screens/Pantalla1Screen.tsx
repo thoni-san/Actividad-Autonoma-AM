97b5c3655e5d3300f739eb60862b7e5f8b16f4bd
Anthoni Zamora <anthoni.zamora@gmail.com>
2025-08-08 22:06:17 -0500

    Actividad Autonoma 1

---
+++ src/screens/Pantalla1Screen.tsx
@@ -0,0 +1,48 @@
+import React from 'react';
+import { Text, Image, TouchableOpacity, SafeAreaView, StyleSheet } from 'react-native';
+import { DrawerScreenProps } from '@react-navigation/drawer';
+import { RootDrawerParams } from '../navigator/DrawerPersonalizado';
+import { styles } from '../theme/appTheme';
+
+type Props = DrawerScreenProps<RootDrawerParams, 'Pantalla1Screen'>;
+
+export const Pantalla1Screen = ({ navigation }: Props) => {
+  return (
+    <SafeAreaView style={styles.container}>
+      <Text style={styles.title}>Bienvenid@s</Text>
+      <Text style={styles.subtitle}>^^</Text>
+      <Image
+        source={require('../img/logo2.png')}
+        style={styles.image}
+      />
+      <TouchableOpacity
+        style={localStyles.button}
+        onPress={() => navigation.navigate('Pantalla2Screen')}
+      >
+        <Text style={localStyles.buttonText}>Acceder</Text>
+      </TouchableOpacity>
+    </SafeAreaView>
+  );
+};
+
+const localStyles = StyleSheet.create({
+  button: {
+    backgroundColor: '#ff6f61',
+    paddingVertical: 18,
+    paddingHorizontal: 50,
+    borderRadius: 30,
+    shadowColor: "#ff6f61",
+    shadowOffset: {
+      width: 0,
+      height: 5,
+    },
+    shadowOpacity: 0.3,
+    shadowRadius: 5,
+    elevation: 8,
+  },
+  buttonText: {
+    color: '#ffffff',
+    fontSize: 18,
+    fontWeight: 'bold',
+  },
+});
